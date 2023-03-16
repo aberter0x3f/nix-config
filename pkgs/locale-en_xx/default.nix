09dc0c76a87cdd94c2f473bc7decb33cbc8da9aa
yzy-1 <50034950+yzy-1@users.noreply.github.com>
2023-03-16 10:53:32 +0800

    Add locale-en_xx

---
+++ pkgs/locale-en_xx/default.nix
@@ -0,0 +1,27 @@
+{ lib, stdenvNoCC, fetchurl, ... }:
+
+stdenvNoCC.mkDerivation rec {
+  pname = "locale-en_xx";
+  version = "2017";
+
+  src = fetchurl {
+    url = "https://xyne.dev/projects/locale-en_xx/src/locale-en_xx-${version}.tar.xz";
+    hash = "sha256-tHnpczSat4jPuWe6G0Q12qouumXjlUMNb6rBKQvfIog=";
+  };
+
+  patches = [
+    ./force-24h.patch
+  ];
+
+  installPhase = ''
+    runHook preInstall
+    install -Dm644 "en_XX@POSIX" "$out/share/i18n/locales/en_XX@POSIX"
+    runHook postInstall
+  '';
+
+  meta = {
+    description = "A mixed international English locale with ISO and POSIX formats for cosmopolitan coders.";
+    homepage = "https://xyne.dev/projects/locale-en_xx";
+    license = lib.licenses.gpl2;
+  };
+}
